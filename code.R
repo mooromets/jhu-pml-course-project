a912d39e8679d882c32cace55fe309a9fe6f8a47
mooromets <sergey.sambor@gmail.com>
2017-09-11 12:34:12 +0200

    data cleaning reshaping

--- code.R
+++ code.R
@@ -8,6 +8,24 @@ fileTesting <- "testing.csv"
 data <- read.csv(fileTraining, na.strings=c("NA", "", "#DIV/0!"))
 
 ## Exploratory analysis
+# many columns include a mostly NAs
+unlist(lapply(1:160, function(i) {sum(is.na(data[, i]))}))
+# these columns are
+colnames(data)[unlist(lapply(1:160, function(i) {if (sum(is.na(data[, i])) > dim(data)[1] * .95) i }))]
+# these variables are explained by a variable new_window, which are 'yes' for the rows that
+# contain not-NA values in these columns. With the names min, max, avg, var, stddev etc. 
+# it's obvious that these variables are summarising ones. Therefore, we remove these
+# variables, rows where new_window is 'yes' and new_window variable itself
+library(dplyr)
+data <- data %>%
+  filter(new_window == "no") %>%
+  select(-unlist(lapply(1:160, function(i) {if (sum(is.na(data[, i])) > dim(data)[1] * .95) i })),
+         -new_window) %>%
+  # replace three columns for datetime with a single one (numeric)
+  mutate(raw_timestamp = as.double(raw_timestamp_part_1 * 10^6 + raw_timestamp_part_2)) %>%
+  select (-raw_timestamp_part_1, -raw_timestamp_part_2, -cvtd_timestamp)
+
+
 
 library(caret)
 set.seed(1377)
